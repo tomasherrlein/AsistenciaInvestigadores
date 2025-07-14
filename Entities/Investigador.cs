505cee2542b1a6c39edc04cc61dd8ef0368e9fa0
tomasherrlein <herrlein.tomas@gmail.com>
2025-07-14 11:43:55 -0300

    agregar funcionalidad a FormAsistencias

--- Entities/Investigador.cs
+++ Entities/Investigador.cs
@@ -1,13 +1,16 @@
-﻿namespace Entities
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Entities
 {
     public class Investigador
     {
         public int IdInvestigador {  get; set; }
         public string Nombre { get; set; }
         public bool Eliminado { get; set; }
-
         public ICollection<Asistencia> Asistencia { get; set; } = new List<Asistencia>();
-
         public ICollection<Departamento> Iddepartamentos { get; set; } = new List<Departamento>();
+
+        public TimeSpan GetTiempoTotalAsistido() 
+            => Asistencia.Aggregate(TimeSpan.Zero, (acumulador, asistencia) => acumulador + asistencia.GetTiempoEmpleado());
     }
 }
